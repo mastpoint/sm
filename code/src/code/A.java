e1126f13381333b8290272215d5ac0f180f910aa
mastpoint <mastpoint@gmail.com>
2022-05-09 10:28:21 +0530

    code change

--- code/src/code/A.java
+++ code/src/code/A.java
@@ -2,7 +2,13 @@ package code;
 
 public class A {
 	
-	public void show(){
+	
+	
+	public void show() {
+		System.out.println("Hello sir");
+	}
+	public void show1(){
+		System.out.println("git practice");
 		System.out.println("Hello");
 	}
 
