7b6277700c8d9da02e9a3993a62d25c62c5c4b1c
mastpoint <mastpoint@gmail.com>
2022-05-23 13:26:41 +0530

    merge conflict

--- code/src/code/A.java
+++ code/src/code/A.java
@@ -2,14 +2,23 @@ package code;
 
 public class A {
 	
-	
-	
-	public void show() {
-		System.out.println("Hello sir");
-	}
+
 	public void show1(){
 		System.out.println("git practice");
+	}
+	public void show(){
+		System.out.println("New");
 		System.out.println("Hello");
+	}
+	
+	public void display1() {
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+
 	}
 
 }
